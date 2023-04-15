d42d7bd90e03e0e4b0173b41f0f7e67570e13efe
abbasi-ali <a.abbasi1376@gmail.com>
2023-04-14 20:16:48 -0400

    ewc commit

--- ewc_utils.py
+++ ewc_utils.py
@@ -1,44 +1,43 @@
 import torch 
 from torch.nn import functional as F 
+from utils import * 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def register_ewc_params(model, dl, bs, noise=None):
-    eps_= []
-
-    if noise != None:
-        eps_ = noise
+def register_ewc_params(model, dl, loss_type, t_cnt=None, multihead=False):
 
     norm_fact = len(dl)
     
     for b_ind, (x, y) in enumerate(dl):
-        x, y = x.reshape(-1, 784).to(device), y.to(device)
-        x = 2*x - 1
+        x, y = x.to(device), y.squeeze().to(device)
 
-        if noise != None:
-            noise_for_batch = eps_[b_ind*bs:min(eps_.shape[0], (b_ind+1)*bs)].to(device)
-            x_tilt = torch.clamp(x + noise_for_batch, -1, 1)
-        else:
-            x_tilt = x 
+        preds = model(x)
+        if multihead:
+            preds = preds[t_cnt]
 
-        preds = model(x_tilt)
-        loss = F.nll_loss(F.log_softmax(preds, dim=1), y)
+        if loss_type == "CE":
+            loss = F.nll_loss(F.log_softmax(preds, dim=1), y)
+        elif loss_type == "focal":
+            criterion = FocalLoss(num_classes=34, gamma=3, alpha=1).to(device)
+            loss = criterion(preds, y)
 
         model.zero_grad()
         loss.backward()
 
         for p_ind, (n, p) in enumerate(model.named_parameters()):
-            if hasattr(model, f"fisher_{n.replace('.', '_')}"):
-                current_fisher = getattr(model, f"fisher_{n.replace('.', '_')}")
-            else:
-                current_fisher = 0
+            if 'heads' not in n:
+                if hasattr(model, f"fisher_{n.replace('.', '_')}"):
+                    current_fisher = getattr(model, f"fisher_{n.replace('.', '_')}")
+                else:
+                    current_fisher = 0
 
-            new_fisher = current_fisher + p.grad.detach() ** 2 / norm_fact 
-            model.register_buffer(f"fisher_{n.replace('.', '_')}", new_fisher)
+                new_fisher = current_fisher + p.grad.detach() ** 2 / norm_fact 
+                model.register_buffer(f"fisher_{n.replace('.', '_')}", new_fisher)
 
     for p_ind, (n, p) in enumerate(model.named_parameters()):
-        model.register_buffer(f"mean_{n.replace('.', '_')}", p.data.clone())
+        if 'heads' not in n:
+            model.register_buffer(f"mean_{n.replace('.', '_')}", p.data.clone())
 
     model.zero_grad()
 
@@ -46,8 +45,9 @@ def register_ewc_params(model, dl, bs, noise=None):
 def compute_ewc_loss(model):
     loss = 0
     for n, p in model.named_parameters():
-        loss += (getattr(model, f"fisher_{n.replace('.', '_')}") * \
-            (p - getattr(model, f"mean_{n.replace('.', '_')}")).pow(2)).sum()
+        if 'heads' not in n:
+            loss += (getattr(model, f"fisher_{n.replace('.', '_')}") * \
+                (p - getattr(model, f"mean_{n.replace('.', '_')}")).pow(2)).sum()
 
     return loss / 2.
 
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 @torch.no_grad()
-def eval(model, ds_tst, verbose=True, end_eval=None):
+def eval(model, ds_tst, verbose=True, end_eval=None, multihead=False, t_cnt=None):
     bs = 128
     dl_tst = DataLoader(ds_tst, batch_size=bs, shuffle=True)
 
@@ -26,6 +26,9 @@ def eval(model, ds_tst, verbose=True, end_eval=None):
             image, mask = data
             image, mask = image.to(device), mask.squeeze().to(device)
             output = model(image)
+            if multihead:
+                output = output[t_cnt]
+
             pred = output.argmax(dim=1)
 
             
@@ -373,12 +376,152 @@ def train_cont_weathers_vanilla(save_name, loss='CE', seed=0):
 
 
 
+def train_cont_cities_ewc(data_root, save_name, loss_type='CE', seed=0, multihead=False):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    weight_ewc = 5e3
+    task_num = 6
+
+    if not os.path.exists(f'./{save_name}'):
+        os.makedirs(save_name)
+
+    save_path = os.path.join(os.getcwd(), save_name)
+
+    vgg_model = VGGNet(model='vgg19', requires_grad=True)
+
+    if multihead:
+        model = FCN8sMH(pretrained_net=vgg_model, n_class=34, n_tasks=task_num).to(device)
+    else:
+        model = FCN8s(pretrained_net=vgg_model, n_class=34).to(device)
+
+    nepoch = 20
+    bs = 16
+    lr=1e-3
+    save_every = 5
+    eval_every = 10
+    
+
+    transform = transforms.Compose([
+        transforms.Resize((256, 512))
+    ])
+    
+    home_path = os.path.expanduser('~')
+    data_root = os.path.join(home_path, 'data', data_root)
+    label_root = os.path.join(home_path, 'data', 'gtFine')
+
+    # city_splits = get_city_task_splits(data_root, task_num)
+    city_splits = get_city_task_splits(data_root, 18) #one city per task
+
+    print(f'Task splits are {city_splits}')
+
+    if loss_type == 'CE':
+        criterion = torch.nn.CrossEntropyLoss().to(device)
+    elif loss_type == 'focal':
+        criterion = FocalLoss(num_classes=34, gamma=3, alpha=1).to(device)
+
+    acc_mat = np.zeros((task_num, task_num))
+
+    train_datasets, test_datasets = [], []
+
+    for t_cnt in range(task_num):
+        ds_train = CityScapeDatasetWithinCitySplit(data_root=data_root, label_root=label_root, 
+                                    transform=transform, split='train', city_names=city_splits[t_cnt], seed=seed)
+        
+        ds_test = CityScapeDatasetWithinCitySplit(data_root=data_root, label_root=label_root, transform=transform, 
+                                   split='test', city_names=city_splits[t_cnt], seed=seed)
+        
+        train_datasets.append(ds_train)
+        test_datasets.append(ds_test)
+
+    
+    for t_cnt in range(task_num):
+        dl_train = DataLoader(train_datasets[t_cnt], batch_size=bs, shuffle=True)
+        
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        loss_ = []
+
+        for epoch in range(nepoch):
+            for i, data in enumerate(dl_train):
+                image, mask = data
+                image, mask = image.to(device), mask.squeeze().to(device)           
+
+                optimizer.zero_grad()
+                output = model(image)
+                if multihead:
+                    output = output[t_cnt]
+
+                loss_critic = criterion(output, mask)
+                if t_cnt > 0:    
+                    loss_ewc = compute_ewc_loss(model)
+                else:
+                    loss_ewc = 0
+
+                loss = loss_critic + loss_ewc * weight_ewc
+
+                loss.backward()
+                optimizer.step()
+
+                loss_.append(loss.item())
+
+                if (i+1) % eval_every == 0 or (i+1) == len(dl_train):
+                    __ = eval(model, test_datasets[t_cnt], verbose=True, 
+                              multihead=multihead, t_cnt=t_cnt)
+                    
+                    torch.save(model.state_dict(), f'{save_path}/{save_name}_model_t{t_cnt}.pth')
+
+                if (i+1) % save_every == 0 or (i+1) == len(dl_train):
+                    print('Epoch: {}, Iter: {}, Loss: {}'.format(epoch, i, loss.item()))
+                    
+                    plt.plot(loss_)
+                    plt.savefig(f'{save_path}/{save_name}_training_loss.png')
+                    plt.close()
+
+                    fig, ax = plt.subplots(3, 5, figsize=(20, 10))
+                    #first row: input images second row: ground truth third row: predicted
+                    #title accordingly
+                    for i in range(5):
+                        ax[0, i].imshow(image[i].cpu().detach().permute(1, 2, 0))
+                        ax[0, i].set_title('input')
+
+                        ax[1, i].imshow(mask[i].cpu().detach(), cmap='viridis', vmin=0, vmax=33)
+                        ax[1, i].set_title('ground truth')
+
+                        ax[2, i].imshow(output[i].cpu().detach().argmax(dim=0), cmap='viridis', vmin=0, vmax=33)
+                        ax[2, i].set_title('predicted')
+
+                    plt.savefig(f'{save_path}/{save_name}_training_results.png')
+                    plt.close()
+
+
+
+        register_ewc_params(model, dl_train, loss_type=loss_type, multihead=multihead, t_cnt=t_cnt)
+        
+        for eval_cnt in range(task_num):            
+            acc_mat[t_cnt, eval_cnt]  = eval(model, test_datasets[eval_cnt], 
+                                             verbose=False, multihead=multihead, t_cnt=t_cnt)
+
+        np.save(f'{save_path}/{save_name}_acc_mat.npy', acc_mat)
+        
+        with np.printoptions(precision=2):
+            print(acc_mat)
+
+    print(f'Task splits are {city_splits}')
+
+
+
 # train_one_task('leftImg8bit', 'clean_all_cities_FSunet')
 
 # train_one_task('leftImg8bit', 'clean_all_cities_VGG19') 
 # train_one_task('leftImg8bit', 'clean_all_cities_VGG19_focal_loss', loss='focal') 
 
 # train_cont_cities_vanilla('leftImg8bit', 'clean_all_cities_VGG19_focalloss_cont_vanilla_t6_3pt', loss='focal')
+
+# train_cont_cities_ewc('leftImg8bit', 'clean_all_cities_VGG19_focalloss_ewc_t6_1pt', loss_type='focal')
+
+# train_cont_cities_ewc('leftImg8bit', 'clean_all_cities_VGG19_focalloss_ewc_t6_1pt_multihead', 
+#                       loss_type='focal', multihead=True)
 
 # train_cont_weathers_vanilla('weathers_VGG19_focalloss_cont_vanilla_t3', loss='focal')
 
